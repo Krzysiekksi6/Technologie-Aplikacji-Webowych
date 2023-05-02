b50a47be9f77179198163c9c7e334535c5957004
Krzysiek <krzysiekksi6@gmail.com>
2023-05-02 15:58:24 +0200

    lab_07

--- Lab_06/app/config.js
+++ Lab_06/app/config.js
@@ -1,5 +1,5 @@
 const config = {
-  port: process.env.PORT || 3000,
+  port: process.env.PORT || 3001,
   databaseUrl: process.env.MONGODB_URI || '',
   JwtSecret: process.env.JWT_SECRET || 'secret'
 };
---
+++ Lab_07/TAW_L7-main/app/business/business.container.js
@@ -0,0 +1,11 @@
+'use strict';
+
+function getter(manager, request) {
+  return function () {
+    return manager.create(request, this);
+  };
+}
+
+export default {
+
+};
